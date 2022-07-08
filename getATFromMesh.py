a45af12579c2f92385044d7947a75343452403c1
lino202 <maxi.rosales202@gmail.com>
2022-07-08 17:19:00 +0200

    simple changes in plots in volumetric ATs

--- getATFromMesh.py
+++ getATFromMesh.py
@@ -2,7 +2,7 @@ import os
 import argparse
 from tqdm import tqdm
 import numpy as np
-from utils import calculateBoxPlotParams
+from utils import calculateBoxPlotParams, plotHistAndBoxPlotSeaBorn
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description="Options")
@@ -19,13 +19,4 @@ boxplotData = calculateBoxPlotParams(lats)
 print("Useful Data: mean-std {0:.2f} +/- {1:.2f}".format(np.mean(lats), np.std(lats)))
 print("\t median {0}, lowQuart {1}, upQuart {2}, lowWhisker {3}, upWhisker {4}".format(boxplotData[0], boxplotData[1], boxplotData[2], boxplotData[3], boxplotData[4]))
 
-fig, axs = plt.subplots(1,2)
-plt.subplots_adjust(wspace=0.4)
-axs[0].hist(lats)
-axs[1].boxplot(lats)
-axs[0].set_ylabel("Frequencies")
-axs[0].set_xlabel("AT [ms]")
-axs[1].set_xlabel("Samples")
-axs[1].set_ylabel("AT [ms]")
-plt.savefig(os.path.join(args.resPath,'AT_metrics.png'))
-plt.show()
+plotHistAndBoxPlotSeaBorn(lats[~np.isnan(lats)], "AT (ms)", path=os.path.join(args.resPath, "AT_metrics_volume.png"))

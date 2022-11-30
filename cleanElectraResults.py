e5790e48d71f1f895174ece9390bb50e1cf033fe
lino202 <maxi.rosales202@gmail.com>
2022-11-30 12:31:51 +0100

    Added CV samples for validation, cleaned results for electra with decimal dt and getFassinaResults to avoid scar regions in calculations

---
+++ cleanElectraResults.py
@@ -0,0 +1,56 @@
+import os
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt 
+import time
+from tqdm import tqdm
+from shutil import copyfile
+import random
+
+parser = argparse.ArgumentParser(description="Options")
+parser.add_argument('--dataPath',type=str, required=True, help='path to data')
+parser.add_argument('--animationPrefix',type=str, required=True)
+parser.add_argument('--timeStart',type=float, required=True)
+parser.add_argument('--timeEnd',type=float, required=True)
+args = parser.parse_args()
+
+
+#Adjust .case for seeing new apd data
+with open(os.path.join(args.dataPath, "{}.case".format(args.animationPrefix)), 'r') as f:
+    data = f.readlines()
+
+caseTimeIdx = data.index("TIME\n")
+dt = np.array(data[caseTimeIdx+1].split(':')[-1].split('\n')[0]).astype(float)
+nSteps = np.array(data[caseTimeIdx+2].split(':')[-1].split('\n')[0]).astype(int)
+caseVariableIdx = data.index("VARIABLE\n")
+ensNames = data[caseVariableIdx+1].split(':')[-1].split('\n')[0].split(" ")[2].split("*")[0]
+
+times = np.arange(args.timeStart, args.timeEnd+dt, dt)
+times = (times * 1/dt).astype(int)     # paraview has problems with this being decimals maybe read ensight manual            
+newdata = data[:caseTimeIdx+5]
+newdata[caseTimeIdx+1] = 'time set: 1\n'
+newdata[caseTimeIdx+2] = 'number of steps: {0:d}\n'.format(times.shape[0])
+newdata[caseTimeIdx+3] = 'filename start number: {0:d}\n'.format(times[0])
+
+timeWriteStep = 10
+for idx in range(0,times.shape[0],timeWriteStep):
+    if idx == 0:
+        tmp = "time values: " + str(times[idx:idx+timeWriteStep]).split("[")[1].split("]")[0] + "\n"
+        newdata.append(tmp)
+    else:
+        tmp = "             " + str(times[idx:idx+timeWriteStep]).split("[")[1].split("]")[0] + "\n"
+        newdata.append(tmp)
+
+print("Removing .ens solutions")
+filesInfoString = newdata[caseTimeIdx-3]
+files2DeletePrefix = filesInfoString.split('*')[0].split(" ")[-1]
+nFiles = int(data[caseTimeIdx+2].split("\n")[0].split(" ")[-1])
+allTimes = np.arange(0, nFiles)
+nAsterisks = newdata[caseTimeIdx-3].count('*')
+times2Delete = allTimes[~np.isin(allTimes, times)]
+for time in times2Delete:
+    os.remove(os.path.join(args.dataPath, "{0}{1}.ens".format(files2DeletePrefix, str(time).zfill(nAsterisks))))
+    # print(os.path.join(args.dataPath, "{0}{1}.ens".format(files2DeletePrefix, str(time).zfill(nAsterisks))))
+
+with open(os.path.join(args.dataPath, "{}.case".format(args.animationPrefix)), 'w') as f:
+    f.writelines(newdata)
--- getFassinaResults.py
+++ getFassinaResults.py
@@ -6,7 +6,7 @@ import numpy as np
 import meshio
 import matplotlib.pyplot as plt
 import random
-from utils import getLocalCvVanillaMesh
+from utilsCV import getLocalCvVanillaMesh
 import pandas as pd
 
 np.seterr(divide='raise', invalid='raise')
@@ -16,9 +16,10 @@ parser.add_argument('--meshPath',type=str, default="F:/Simulations/electra_sims/
 # parser.add_argument('--id',type=str, required=True, help='path to data')
 parser.add_argument('--timeStart',type=int, default=2000)
 parser.add_argument('--timeEnd',type=int, default=3000)
+parser.add_argument('--dt',type=float, default=1.)
 parser.add_argument('--apd',type=int, default=90)
 parser.add_argument('--spaceUnit',type=str, default="mm")
-parser.add_argument('--maxDist', type=float, help='distance radio', default=0.5)
+parser.add_argument('--maxDist', type=float, help='distance radius', default=0.5)
 parser.add_argument('--maxCV', type=float, help='max CV in cm/s', default=300)
 parser.add_argument('--resExcel', type=str, help='max CV in cm/s', default="F:/Simulations/electra_sims/Biovad/Paper/fassina_05_thick/results.xlsx")
 args = parser.parse_args()
@@ -57,15 +58,16 @@ res["ATP mean"] = np.nanmean(lats[idxpatch])
 res["ATP median"] = np.nanmedian(lats[idxpatch])
 res["ATP min"] = np.nanmin(lats[idxpatch])
 res["ATP max"] = np.nanmax(lats[idxpatch])
-# print("selected idx AT is : {0:f}".format(lats[args.idxAT]) )
-# print("Mean AT Myo {0:f}".format(np.nanmean(lats[idxmyo])))
-# print("Median AT Myo {0:f}".format(np.nanmedian(lats[idxmyo])))
-# print("Min AT Myo {0:f}".format(np.nanmin(lats[idxmyo])))
-# print("Max AT Myo {0:f}".format(np.nanmax(lats[idxmyo])))
-# print("Mean AT Patch {0:f}".format(np.nanmean(lats[idxpatch])))
-# print("Median AT Patch {0:f}".format(np.nanmedian(lats[idxpatch])))
-# print("Min AT Patch {0:f}".format(np.nanmin(lats[idxpatch])))
-# print("Max AT Patch {0:f}".format(np.nanmax(lats[idxpatch])))
+
+
+print("ATM mean:   {0:f}".format(np.nanmean(lats[idxmyo])))
+print("ATM median: {0:f}".format(np.nanmedian(lats[idxmyo])))
+print("ATM min:    {0:f}".format(np.nanmin(lats[idxmyo])))
+print("ATM max:    {0:f}".format(np.nanmax(lats[idxmyo])))
+print("ATP mean:   {0:f}".format(np.nanmean(lats[idxpatch])))
+print("ATP median: {0:f}".format(np.nanmedian(lats[idxpatch])))
+print("ATP min:    {0:f}".format(np.nanmin(lats[idxpatch])))
+print("ATP max:    {0:f}".format(np.nanmax(lats[idxpatch])))
 
 #APD90 ----------------------------------------------------------------------------
 print("Calculating APD")
@@ -77,14 +79,14 @@ for i in tqdm(range(args.timeStart, args.timeEnd)):
     if i == args.timeStart: v = np.zeros((tmp.shape[0], args.timeEnd - args.timeStart))
     v[:, i-args.timeStart] = tmp
 
-#Plot some curves
-# fig, ax = plt.subplots()
-# for i in range(10):
-#     plotIdx = random.randint(0,v.shape[0])
-#     ax.plot(v[plotIdx,:], label=plotIdx)
-# ax.legend()
-# ax.set_title("10 Vm signals")
-# plt.show()
+# Plot some curves
+fig, ax = plt.subplots()
+for i in range(10):
+    plotIdx = random.randint(0,v.shape[0])
+    ax.plot(v[plotIdx,:], label=plotIdx)
+ax.legend()
+ax.set_title("10 Vm signals")
+plt.show()
 
 diff = np.diff(v, axis=1)
 upstrokeIdxs = np.argmax(diff, axis=1)
@@ -103,6 +105,7 @@ for i in tqdm(range(v.shape[0])):
 tin = tin + peakIdxs 
 apds = tin - upstrokeIdxs
 apds[apds<0] = np.nan
+apds = apds * args.dt
 
 res["APD90M mean"] = np.nanmean(apds[idxmyo])
 res["APD90M median"] = np.nanmedian(apds[idxmyo])
@@ -112,6 +115,16 @@ res["APD90P mean"] = np.nanmean(apds[idxpatch])
 res["APD90P median"] = np.nanmedian(apds[idxpatch])
 res["APD90P min"] = np.nanmin(apds[idxpatch])
 res["APD90P max"] = np.nanmax(apds[idxpatch])
+
+
+print("APD90M mean:   {0:f}".format(np.nanmean(apds[idxmyo])))
+print("APD90M median: {0:f}".format(np.nanmedian(apds[idxmyo])))
+print("APD90M min:    {0:f}".format(np.nanmin(apds[idxmyo])))
+print("APD90M max:    {0:f}".format(np.nanmax(apds[idxmyo])))
+print("APD90P mean:   {0:f}".format(np.nanmean(apds[idxpatch])))
+print("APD90P median: {0:f}".format(np.nanmedian(apds[idxpatch])))
+print("APD90P min:    {0:f}".format(np.nanmin(apds[idxpatch])))
+print("APD90P max:    {0:f}".format(np.nanmax(apds[idxpatch])))
 
 # CVs --------------------------------------------------------------------
 points = mesh.points
@@ -144,10 +157,15 @@ res["CVP median"] = np.nanmedian(CVmagnitudes[idxpatch])
 res["CVP min"] = np.nanmin(CVmagnitudes[idxpatch])
 res["CVP max"] = np.nanmax(CVmagnitudes[idxpatch])
 
-# print("Mean CVmag {0:f}".format(np.nanmean(CVmagnitudes[idxpatch])))
-# print("Median CVmag {0:f}".format(np.nanmedian(CVmagnitudes[idxpatch])))
-# print("Min CVmag {0:f}".format(np.nanmin(CVmagnitudes[idxpatch])))
-# print("Max CVmag {0:f}".format(np.nanmax(CVmagnitudes[idxpatch])))
+
+print("CVM mean:   {0:f}".format(np.nanmean(CVmagnitudes[idxmyo])))
+print("CVM median: {0:f}".format(np.nanmedian(CVmagnitudes[idxmyo])))
+print("CVM min:    {0:f}".format(np.nanmin(CVmagnitudes[idxmyo])))
+print("CVM max:    {0:f}".format(np.nanmax(CVmagnitudes[idxmyo])))
+print("CVP mean:   {0:f}".format(np.nanmean(CVmagnitudes[idxpatch])))
+print("CVP median: {0:f}".format(np.nanmedian(CVmagnitudes[idxpatch])))
+print("CVP min:    {0:f}".format(np.nanmin(CVmagnitudes[idxpatch])))
+print("CVP max:    {0:f}".format(np.nanmax(CVmagnitudes[idxpatch])))
 
 # RTs--------------------------------------------------------------------
 rts = lats + apds
@@ -167,13 +185,24 @@ res["RTGP median"] = np.nanmedian(RTgradients[idxpatch])
 res["RTGP min"] = np.nanmin(RTgradients[idxpatch])
 res["RTGP max"] = np.nanmax(RTgradients[idxpatch])
 
-# print("Mean RTgrad {0:f}".format(np.nanmean(RTgradients[idxpatch])))
-# print("Median RTgrad {0:f}".format(np.nanmedian(RTgradients[idxpatch])))
-# print("Min RTgrad {0:f}".format(np.nanmin(RTgradients[idxpatch])))
-# print("Max RTgrad {0:f}".format(np.nanmax(RTgradients[idxpatch])))
+print("RTGM mean:   {0:f}".format(np.nanmean(RTgradients[idxmyo])))
+print("RTGM median: {0:f}".format(np.nanmedian(RTgradients[idxmyo])))
+print("RTGM min:    {0:f}".format(np.nanmin(RTgradients[idxmyo])))
+print("RTGM max:    {0:f}".format(np.nanmax(RTgradients[idxmyo])))
+print("RTGP mean:   {0:f}".format(np.nanmean(RTgradients[idxpatch])))
+print("RTGP median: {0:f}".format(np.nanmedian(RTgradients[idxpatch])))
+print("RTGP min:    {0:f}".format(np.nanmin(RTgradients[idxpatch])))
+print("RTGP max:    {0:f}".format(np.nanmax(RTgradients[idxpatch])))
+
 #Save--------------------------------------------------------------------
 point_data = {"ATs_(ms)": lats, "APD{}_(ms)".format(args.apd): apds, "CVMag_(cm/s)": CVmagnitudes, "CVversors": CVversors, "RTs_(ms)": rts}
 point_data["RTgrad_[ms/mm]"] = RTgradients
+
+# Delete scar nodes from results
+idxscar = mesh.point_sets["scar_nodes"]
+for key in point_data.keys():
+    point_data[key][idxscar] = np.nan
+
 myo_nodes = np.zeros(points.shape[0])
 myo_nodes[idxmyo] = 1
 patch_nodes = np.zeros(points.shape[0])
@@ -183,8 +212,8 @@ point_data["patch_nodes"] = patch_nodes
 meshOut = meshio.Mesh(mesh.points, mesh.cells, point_data=point_data)
 meshOut.write(os.path.join(myResPath, "myresults.vtk"))
 
-df = pd.read_excel(args.resExcel)
-res["Id"] = "_".join(args.mainPath.split("/")[-2:])
-new_row = pd.Series(res)
-df2 = pd.concat([df, new_row.to_frame().T], ignore_index=True)
-df2.to_excel(args.resExcel)
+# df = pd.read_excel(args.resExcel)
+# res["Id"] = "_".join(args.mainPath.split("/")[-2:])
+# new_row = pd.Series(res)
+# df2 = pd.concat([df, new_row.to_frame().T], ignore_index=True)
+# df2.to_excel(args.resExcel)

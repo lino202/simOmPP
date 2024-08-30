b8ea336e44969e32ade4f2a47eafa614a022c46a
lino202 <maxi.rosales202@gmail.com>
2024-08-30 20:11:34 +0200

    Added plots for Manu Perth

--- getPlotsOMsPerth.py
+++ getPlotsOMsPerth.py
@@ -6,7 +6,7 @@ import argparse
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-
+import copy
 
 parser = argparse.ArgumentParser(description="Options")
 parser.add_argument('--excelFilePath',type=str, required=True, help='path to data')
@@ -23,71 +23,215 @@ plt.rcParams.update({'mathtext.default':  'regular' })
 
 
 df = pd.read_excel(args.excelFilePath, sheet_name=args.sheetName, header=args.headerRow)
-df = df.loc[df['Region'] == 'overall']
 df = df.loc[df['Sample'] != 'DP1']
-# df = df.loc[df['Region'] != 'M']
-df = df.loc[df['CL (ms)'] == 2000]
-df = df.loc[df['Stim'] == 'Trans']
-# CV------------------------------------------
+df = df.loc[df['Region'] != 'M']
+
+
+# # Pore size -------------------------------------------------------------------------------------------------------
+# # Basal  ---------------------------------
+# tmp_df = copy.deepcopy(df)
+# tmp_df = tmp_df.loc[tmp_df['Region'] != 'overall']
+# tmp_df = tmp_df.loc[tmp_df['Stim'] == 'Basal']
 
 # plt.figure()
-# sns.catplot(data=df, x="Stim Type", y="median CV", hue="Region")
-# plt.savefig(os.path.join(args.outFolder, "CV_stimtype.png"), dpi=500)
-
-
-# plt.figure()
-# sns.catplot(data=df, x="CL (ms)", y="median CV", hue="Region")
-# plt.savefig(os.path.join(args.outFolder, "CV_cl.png"), dpi=500)
-
+# sns.catplot(data=tmp_df, x="Sample", y="median CV", hue="Region", palette=sns.color_palette(['darkgreen', 'limegreen']), s=70)
+# plt.savefig(os.path.join(args.outFolder, "CV_basal.png"), dpi=500)
 
 # plt.figure()
-# sns.catplot(data=df, x="Stim", y="median CV", hue="Region")
-# plt.savefig(os.path.join(args.outFolder, "CV_stim.png"), dpi=500)
+# sns.catplot(data=tmp_df, x="Sample", y="median APD90", hue="Region", palette=sns.color_palette(['darkgreen', 'limegreen']), s=70)
+# plt.savefig(os.path.join(args.outFolder, "APD_basal.png"), dpi=500)
 
+# # Stim 1000 ---------------------------------------------------
+# tmp_df = copy.deepcopy(df)
+# tmp_df = tmp_df.loc[tmp_df['Region'] != 'overall']
+# tmp_df = tmp_df.loc[tmp_df['CL (ms)'] == 1000]
+
+# tmp_df['Region_Stim'] = tmp_df['Region'] + '_' + tmp_df['Stim']
 
 # plt.figure()
-# sns.catplot(data=df, x="Sample", y="median CV", hue="Region")
-# plt.savefig(os.path.join(args.outFolder, "CV_sample_poresize.png"), dpi=500)
+# sns.catplot(data=tmp_df, x="Sample", y="median CV", hue="Region_Stim", palette=sns.color_palette(['darkgreen', 'limegreen', 'darkred', 'lightcoral']), s=70)
+# plt.savefig(os.path.join(args.outFolder, "CV_stim_1000.png"), dpi=500)
+
+# plt.figure()
+# sns.catplot(data=tmp_df, x="Sample", y="median APD90", hue="Region_Stim", palette=sns.color_palette(['darkgreen', 'limegreen', 'darkred', 'lightcoral']), s=70)
+# plt.savefig(os.path.join(args.outFolder, "APD_stim_1000.png"), dpi=500)
+
+
+# # Stim 2000 ---------------------------------------------------
+# tmp_df = copy.deepcopy(df)
+# tmp_df = tmp_df.loc[tmp_df['Region'] != 'overall']
+# tmp_df = tmp_df.loc[tmp_df['CL (ms)'] == 2000]
+
+# tmp_df['Region_Stim'] = tmp_df['Region'] + '_' + tmp_df['Stim']
+
+# plt.figure()
+# sns.catplot(data=tmp_df, x="Sample", y="median CV", hue="Region_Stim", palette=sns.color_palette(['darkgreen', 'limegreen', 'darkred', 'lightcoral']), s=70)
+# plt.savefig(os.path.join(args.outFolder, "CV_stim_2000.png"), dpi=500)
+
+# plt.figure()
+# sns.catplot(data=tmp_df, x="Sample", y="median APD90", hue="Region_Stim", palette=sns.color_palette(['darkgreen', 'limegreen', 'darkred', 'lightcoral']), s=70)
+# plt.savefig(os.path.join(args.outFolder, "APD_stim_2000.png"), dpi=500)
+
+# # Pore shape -----------------------------------------------------------------------------------------
+# # Basal 
+# tmp_df = copy.deepcopy(df)
+# tmp_df = tmp_df.loc[tmp_df['Region'] == 'overall']
+# tmp_df = tmp_df.loc[tmp_df['Stim'] == 'Basal']
+
+# plt.figure()
+# sns.catplot(data=tmp_df, x="Sample", y="median CV", hue="Pore", palette=sns.color_palette(['cornflowerblue', 'lightcoral', 'limegreen',]), s=70)
+# plt.savefig(os.path.join(args.outFolder, "CV_basal_shape.png"), dpi=500)
+
+# plt.figure()
+# sns.catplot(data=tmp_df, x="Sample", y="median APD90", hue="Pore", palette=sns.color_palette(['cornflowerblue', 'lightcoral', 'limegreen']), s=70)
+# plt.savefig(os.path.join(args.outFolder, "APD_basal_shape.png"), dpi=500)
+
+
+# # Stim 1000
+# tmp_df = copy.deepcopy(df)
+# tmp_df = tmp_df.loc[tmp_df['Region'] == 'overall']
+# tmp_df = tmp_df.loc[tmp_df['CL (ms)'] == 1000]
+
+# tmp_df['Pore_Stim'] = tmp_df['Pore'] + '_' + tmp_df['Stim']
+
+# plt.figure()
+# sns.catplot(data=tmp_df, x="Sample", y="median CV", hue="Pore_Stim", palette=sns.color_palette(['mediumblue', 'cornflowerblue', 'darkred', 'lightcoral', 'darkgreen', 'limegreen']), s=70)
+# plt.savefig(os.path.join(args.outFolder, "CV_stim_1000_shape.png"), dpi=500)
+
+# plt.figure()
+# sns.catplot(data=tmp_df, x="Sample", y="median APD90", hue="Pore_Stim", palette=sns.color_palette(['mediumblue', 'cornflowerblue', 'darkred', 'lightcoral', 'darkgreen', 'limegreen']), s=70)
+# plt.savefig(os.path.join(args.outFolder, "APD_stim_1000_shape.png"), dpi=500)
+
+
+# # Stim 2000
+# tmp_df = copy.deepcopy(df)
+# tmp_df = tmp_df.loc[tmp_df['Region'] == 'overall']
+# tmp_df = tmp_df.loc[tmp_df['CL (ms)'] == 2000]
+
+# tmp_df['Pore_Stim'] = tmp_df['Pore'] + '_' + tmp_df['Stim']
+
+# plt.figure()
+# sns.catplot(data=tmp_df, x="Sample", y="median CV", hue="Pore_Stim", palette=sns.color_palette(['mediumblue', 'cornflowerblue', 'darkred', 'lightcoral', 'darkgreen', 'limegreen']), s=70)
+# plt.savefig(os.path.join(args.outFolder, "CV_stim_2000_shape.png"), dpi=500)
+
+# plt.figure()
+# sns.catplot(data=tmp_df, x="Sample", y="median APD90", hue="Pore_Stim", palette=sns.color_palette(['mediumblue', 'cornflowerblue', 'darkred', 'lightcoral', 'darkgreen', 'limegreen']), s=70)
+# plt.savefig(os.path.join(args.outFolder, "APD_stim_2000_shape.png"), dpi=500)
+
+# ------------------------------------------------
+print("Getting simpler relative results")
+
+# # Basal  ---------------------------------
+# tmp_df = copy.deepcopy(df)
+# tmp_df = tmp_df.loc[tmp_df['Region'] != 'overall']
+# tmp_df = tmp_df.loc[tmp_df['Stim'] == 'Basal']
+
+# ll = tmp_df.loc[tmp_df['Region'] == 'L'].reset_index()
+# ss = tmp_df.loc[tmp_df['Region'] == 'S'].reset_index()
+
+# tmp_df = tmp_df.drop('Region', axis=1)
+# tmp_df = tmp_df.drop('median CV', axis=1)
+# tmp_df = tmp_df.drop('median APD90', axis=1)
+# tmp_df = tmp_df.drop_duplicates().reset_index()
+
+# tmp_df['percentual relative CV']  = -1*(ll['median CV'] - ss['median CV']) / ll['median CV'] * 100
+# tmp_df['percentual relative APD90']  = (ll['median APD90'] - ss['median APD90']) / ll['median APD90'] * 100
+
+# plt.figure()
+# fig = sns.catplot(data=tmp_df, x="Sample", y="percentual relative CV", palette=sns.color_palette(['limegreen']), s=70)
+# fig.refline(y=0, color='black')
+# plt.savefig(os.path.join(args.outFolder, "relative_CV_basal.png"), dpi=500)
+
+# plt.figure()
+# fig = sns.catplot(data=tmp_df, x="Sample", y="percentual relative APD90", palette=sns.color_palette(['limegreen']), s=70)
+# fig.refline(y=0, color='black')
+# plt.savefig(os.path.join(args.outFolder, "relative_APD_basal.png"), dpi=500)
+
+
+
+# Stim 1000  ---------------------------------
+tmp_df = copy.deepcopy(df)
+tmp_df = tmp_df.loc[tmp_df['Region'] != 'overall']
+tmp_df = tmp_df.loc[tmp_df['CL (ms)'] == 1000]
+
+# tmp_df['Region_Stim'] = tmp_df['Region'] + '_' + tmp_df['Stim']
+
+ll = tmp_df.loc[tmp_df['Region'] == 'L'].reset_index()
+ss = tmp_df.loc[tmp_df['Region'] == 'S'].reset_index()
+
+tmp_df = tmp_df.drop('Region', axis=1)
+tmp_df = tmp_df.drop('median CV', axis=1)
+tmp_df = tmp_df.drop('median APD90', axis=1)
+tmp_df = tmp_df.drop_duplicates().reset_index()
+
+tmp_df['percentual relative CV']  = -1*(ll['median CV'] - ss['median CV']) / ll['median CV'] * 100
+tmp_df['percentual relative APD90']  = (ll['median APD90'] - ss['median APD90']) / ll['median APD90'] * 100
+
+# we delete long and trans by doing the mean this can be commented and hue='Stim' added but for me is better this way
+ll = tmp_df.loc[tmp_df['Stim'] == 'Long'].reset_index()
+ss = tmp_df.loc[tmp_df['Stim'] == 'Trans'].reset_index()
+
+tmp_df = tmp_df.drop('Stim', axis=1)
+tmp_df = tmp_df.drop('index', axis=1)
+tmp_df = tmp_df.drop('percentual relative CV', axis=1)
+tmp_df = tmp_df.drop('percentual relative APD90', axis=1)
+tmp_df = tmp_df.drop_duplicates().reset_index()
+
+tmp_df['percentual relative CV'] = (ll['percentual relative CV']+ss['percentual relative CV'])/2
+tmp_df['percentual relative APD90'] = (ll['percentual relative APD90']+ss['percentual relative APD90'])/2
 
 plt.figure()
-sns.catplot(data=df, x="Sample", y="median CV", hue="Pore")
-plt.savefig(os.path.join(args.outFolder, "CV_sample_poreshape.png"), dpi=500)
-
-# APD----------------------------------------------
-
-
-# plt.figure()
-# sns.catplot(data=df, x="Stim Type", y="median APD90", hue="Region")
-# plt.savefig(os.path.join(args.outFolder, "APD90_stimtype.png"), dpi=500)
-
-
-# plt.figure()
-# sns.catplot(data=df, x="CL (ms)", y="median APD90", hue="Region")
-# plt.savefig(os.path.join(args.outFolder, "APD90_cl.png"), dpi=500)
-
-
-# plt.figure()
-# sns.catplot(data=df, x="Stim", y="median APD90", hue="Region")
-# plt.savefig(os.path.join(args.outFolder, "APD90_stim.png"), dpi=500)
-
-
-# plt.figure()
-# sns.catplot(data=df, x="Sample", y="median APD90", hue="Region")
-# plt.savefig(os.path.join(args.outFolder, "APD90_samplesize.png"), dpi=500)
+fig = sns.catplot(data=tmp_df, x="Sample", y="percentual relative CV", palette=sns.color_palette(['limegreen']), s=70)
+fig.refline(y=0, color='black')
+plt.savefig(os.path.join(args.outFolder, "relative_CV_stim_1000.png"), dpi=500)
 
 plt.figure()
-sns.catplot(data=df, x="Sample", y="median APD90", hue="Pore")
-plt.savefig(os.path.join(args.outFolder, "APD90_sample_poreshape.png"), dpi=500)
-
-
-print("End")
-
+fig = sns.catplot(data=tmp_df, x="Sample", y="percentual relative APD90", palette=sns.color_palette(['limegreen']), s=70)
+fig.refline(y=0, color='black')
+plt.savefig(os.path.join(args.outFolder, "relative_APD_stim_1000.png"), dpi=500)
 
 
 
 
+# Stim 2000  ---------------------------------
+tmp_df = copy.deepcopy(df)
+tmp_df = tmp_df.loc[tmp_df['Region'] != 'overall']
+tmp_df = tmp_df.loc[tmp_df['CL (ms)'] == 2000]
 
+# tmp_df['Region_Stim'] = tmp_df['Region'] + '_' + tmp_df['Stim']
 
+ll = tmp_df.loc[tmp_df['Region'] == 'L'].reset_index()
+ss = tmp_df.loc[tmp_df['Region'] == 'S'].reset_index()
 
+tmp_df = tmp_df.drop('Region', axis=1)
+tmp_df = tmp_df.drop('median CV', axis=1)
+tmp_df = tmp_df.drop('median APD90', axis=1)
+tmp_df = tmp_df.drop_duplicates().reset_index()
+
+tmp_df['percentual relative CV']  = -1*(ll['median CV'] - ss['median CV']) / ll['median CV'] * 100
+tmp_df['percentual relative APD90']  = (ll['median APD90'] - ss['median APD90']) / ll['median APD90'] * 100
+
+# we delete long and trans by doing the mean this can be commented and hue='Stim' added but for me is better this way
+ll = tmp_df.loc[tmp_df['Stim'] == 'Long'].reset_index()
+ss = tmp_df.loc[tmp_df['Stim'] == 'Trans'].reset_index()
+
+tmp_df = tmp_df.drop('Stim', axis=1)
+tmp_df = tmp_df.drop('index', axis=1)
+tmp_df = tmp_df.drop('percentual relative CV', axis=1)
+tmp_df = tmp_df.drop('percentual relative APD90', axis=1)
+tmp_df = tmp_df.drop_duplicates().reset_index()
+
+tmp_df['percentual relative CV'] = (ll['percentual relative CV']+ss['percentual relative CV'])/2
+tmp_df['percentual relative APD90'] = (ll['percentual relative APD90']+ss['percentual relative APD90'])/2
+
+plt.figure()
+fig = sns.catplot(data=tmp_df, x="Sample", y="percentual relative CV", palette=sns.color_palette(['limegreen']), s=70)
+fig.refline(y=0, color='black')
+plt.savefig(os.path.join(args.outFolder, "relative_CV_stim_2000.png"), dpi=500)
+
+plt.figure()
+fig = sns.catplot(data=tmp_df, x="Sample", y="percentual relative APD90", palette=sns.color_palette(['limegreen']), s=70)
+fig.refline(y=0, color='black')
+plt.savefig(os.path.join(args.outFolder, "relative_APD_stim_2000.png"), dpi=500)
     
 
--- scripts/ManuPerth/getResultsFromOmapManuPerth_AR1.bat
+++ scripts/ManuPerth/getResultsFromOmapManuPerth_AR1.bat
@@ -5,35 +5,35 @@
 
 @REM @REM @REM AR1 ----------------------------------------------
 
-@REM python D:/Code/simOmPP/getResultsFromOmapFiltered.py ^
-@REM --folderPath                D:/Data/OM_BioVAD/Perth_24062024/total/AR1 ^
-@REM --videoName                 3 ^
-@REM --fileName                  FileTag003_video_filtered ^
-@REM --matVarName                wholav_images ^
-@REM --getSubRegions             ^
-@REM --subregionNames            L M S ^
-@REM --roisName                  roi_3 ^
-@REM --exposure                  10 ^
-@REM --reverse                   ^
-@REM --startTime                 880 ^
-@REM --endTime                   8030 ^
-@REM --before                    600 ^
-@REM --after                     1050 ^
-@REM --nBeats                    4 ^
-@REM --cleanProcess              0 ^
-@REM --blockDown                 0 ^
-@REM --blockUp                   0 ^
-@REM --shouldNotHaveAllPoints    ^
-@REM --pixRes                    0.0136 ^
-@REM --maxDist                   3 ^
-@REM --maxCV                     300 ^
-@REM --cvCalcMethod              vanilla ^
-@REM --scaleVectors              1. ^
-@REM --apdtype                   90 ^
-@REM --stimCL                    1880 ^
-@REM --stimType                  Basal ^
-@REM --sampleType                Rect ^
-@REM --sampleName                AR1
+python D:/Code/simOmPP/getResultsFromOmapFiltered.py ^
+--folderPath                D:/Data/OM_BioVAD/Perth_24062024/total/AR1 ^
+--videoName                 3 ^
+--fileName                  FileTag003_video_filtered ^
+--matVarName                wholav_images ^
+--getSubRegions             ^
+--subregionNames            L M S ^
+--roisName                  roi_3 ^
+--exposure                  10 ^
+--reverse                   ^
+--startTime                 880 ^
+--endTime                   8030 ^
+--before                    600 ^
+--after                     1050 ^
+--nBeats                    4 ^
+--cleanProcess              0 ^
+--blockDown                 0 ^
+--blockUp                   0 ^
+--shouldNotHaveAllPoints    ^
+--pixRes                    0.0136 ^
+--maxDist                   3 ^
+--maxCV                     300 ^
+--cvCalcMethod              vanilla ^
+--scaleVectors              1. ^
+--apdtype                   90 ^
+--stimCL                    1880 ^
+--stimType                  Basal ^
+--sampleType                Rect ^
+--sampleName                AR1
 
 @REM python D:/Code/simOmPP/getResultsFromOmapFiltered.py ^
 @REM --folderPath                D:/Data/OM_BioVAD/Perth_24062024/total/AR1 ^
